63c1d08ae7a302d468d58f78d77ff3b6c83517f7
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2024-11-08 12:26:49 -0600

    Started temp

---
+++ iPhoneLan/iPhoneLan/iPhoneLanApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPhoneLanApp.swift
+//  iPhoneLan
+//
+//  Created by Betim Hodza on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct iPhoneLanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
